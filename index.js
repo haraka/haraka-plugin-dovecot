ae07307606f49669d740c14e6e21767d9afb8232
April Grimoire <april@aprilg.moe>
2025-11-08 20:05:05 +0800

    Better msg for transaction.results for unix-sockets
    It seems this message is not used later,
    though I am not sure about this due to lack of original comments.

--- index.js
+++ index.js
@@ -122,6 +122,9 @@ exports.get_dovecot_response = function (connection, domain, email, cb) {
     }
   }
 
+  socket_address = options.path ?
+    options.path :
+    `${options.host}:${options.port}`;
   connection.transaction.results.add(plugin, {
     msg: `sock: ${options.host}:${options.port}`,
   });
